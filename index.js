6f5bfdb4642a7a9033eab30a7d637f1ec33eaebc
Le Lam <lamtun1998@gmail.com>
2019-05-14 12:36:42 +0700

    'update'

--- index.js
+++ index.js
@@ -8,10 +8,48 @@ function getLargestImageFromSrcSet(srcSet) {
     return imgSrc;
 }
 
+function extractItems() {
+    const extractedElements = document.querySelectorAll('div._2z6nI > div.Nnq7C.weEfm');
+    const items = [];
+    for (let element of extractedElements) {
+        items.push(element.innerText);
+    }
+    return items;
+}
+
+async function scrapeInfiniteScrollItems(
+    page,
+    extractItems,
+    itemTargetCount,
+    scrollDelay = 1,
+) {
+    let items = [];
+    try {
+        let previousHeight;
+        while (items.length < itemTargetCount) {
+            items = await page.evaluate(extractItems);
+            previousHeight = await page.evaluate('document.body.scrollHeight');
+            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+            await page.waitFor(scrollDelay);
+        }
+    } catch (e) {}
+    return items;
+}
+
 async function getImageUrlsFromPage(url) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
     const page = await browser.newPage();
-    await page.goto(url);
+    page.setViewport({ width: 1280, height: 926 });
+    await page.goto(url, {
+        waitUntil: 'load'
+    });
+    // await timeout(5000);
+
+    const items = await scrapeInfiniteScrollItems(page, extractItems, 100);
 
     const imageSrcSets = await page.evaluate(() => {
         const imgs = Array.from(document.querySelectorAll('article img'));
